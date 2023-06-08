f63f249deedf6466197ce6d6980cecf9d8e29069
Aidar <adrizakirov@gmail.com>
2023-06-08 20:56:38 +0900

    Добавлено описание задачи

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿using static Methods;
 
+string task1 = "Задача: Написать программу, которая из имеющегося массива строк формирует ";
+string task2 = "новый массив из строк, длина которых меньше, либо равна 3 символам.";
+Console.WriteLine(task1 + "\n" + task2);
+
+string[] array = CreateStringArray();
