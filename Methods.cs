a94a512f761a98ab0a5d7dd12dcafc1249815b4b
Aidar <adrizakirov@gmail.com>
2023-06-08 20:48:45 +0900

    Создан метод CreateStringArray

--- Methods.cs
+++ Methods.cs
@@ -13,4 +13,15 @@ public static class Methods
     int number = Convert.ToInt32(Console.ReadLine());
     return number;
   }
+
+  public static string[] CreateStringArray()
+  {
+    int size = InputNumber("Задайте длину массива: ");
+    string[] array = new string[size];
+    for (int i = 0; i < size; i++)
+    {
+      array[i] = InputString($"Введите {i + 1} элемент массива: ");
+    }
+    return array;
+  }
 }
