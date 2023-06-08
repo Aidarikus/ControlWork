0af810e566eb7b92e07b44dfeca89bc8c9a12fdd
Aidar <adrizakirov@gmail.com>
2023-06-08 20:46:21 +0900

    Создан метод InputString

--- Methods.cs
+++ Methods.cs
@@ -1,4 +1,11 @@
 public static class Methods
 {
+    public static string InputString(string text)
+  {
+    Console.Write(text);
+    string element = Console.ReadLine();
+    return element;
+  }
+
   
 }
